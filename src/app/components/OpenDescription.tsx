f77cb7789cb54f66510498baecef23051d90b9e6
McMatthew <matteo.bianchi003@outlook.it>
2023-10-14 19:49:44 +0200

    m

--- src/app/components/OpenDescription.tsx
+++ src/app/components/OpenDescription.tsx
@@ -11,15 +11,3 @@ const OpenDescription = () => {
   );
 };
 export default OpenDescription;
-// Il mio nome è Matteo Bianchi.
-//       <br />
-//       Dal 2022 a questa parte sto dedicando me stesso nel diventare un web
-//       developer a tutto tondo, dal front end al back end.
-//       <br />
-//       Ho avuto modo di utilizzare tecnologie più o meno avanzate in questi anni.
-//       Dal percorso scolastico in C# ai progetti seguiti per lavoro in Kotlin o
-//       React. Questo sito è il mio personale portfolio... creato grazie alle mie
-//       conoscenze di UX/UI e di sviluppo.
-//       <Space h={16} />
-//       Se sei interessato naviga pure tra le sezioni, lì racconto cosa so e quali
-//       strumenti utilizzo ogni giorno...
